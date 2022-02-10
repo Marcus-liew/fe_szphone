04366c1997203168b69d6339df19b70099bf8d98
Wanley <matthw_lew@hotmail.com>
2022-02-10 18:23:54 +0800

    add test

---
+++ test/test.js
@@ -0,0 +1,13 @@
+// function print() {
+//     document.getElementById("demo").innerHTML="RUNOOB!";
+// }
+// setTimeout(print, 3000);
+
+// setTimeout(function () {
+//     document.getElementById("demo").innerHTML="RUNOOB!";
+// }, 3000);
+
+setTimeout(function () {
+    document.getElementById("demo1").innerHTML="RUNOOB-1!";}, 3000);
+    document.getElementById("demo2").innerHTML="RUNOOB-2!";
+    console.log("2");
