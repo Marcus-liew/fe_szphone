4d3eeed5998f6545071872468d010bc124eea8cc
Marcus <matthw_lew@hotmail.com>
2022-02-11 06:24:01 +0800

    add test

--- test/test.js
+++ test/test.js
@@ -7,7 +7,3 @@
 //     document.getElementById("demo").innerHTML="RUNOOB!";
 // }, 3000);
 
-setTimeout(function () {
-    document.getElementById("demo1").innerHTML="RUNOOB-1!";}, 3000);
-    document.getElementById("demo2").innerHTML="RUNOOB-2!";
-    console.log("2");
--- 前端零基础入门/步骤五 JS流程控制语句/1-1 if语句.js
+++
@@ -1,6 +0,0 @@
-var age = prompt("please input your age:");
-if (age > 18){
-    alert("please come in!");
-}else{
-    alert("go out!");
-}
