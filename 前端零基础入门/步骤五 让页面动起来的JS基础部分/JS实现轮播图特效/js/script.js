7a80fa3eb761275e05b27aaf63339ce9e3698558
Marcus <matthw_lew@hotmail.com>
2022-02-15 23:04:50 +0800

    add test

--- 前端零基础入门/步骤五 让页面动起来的JS基础部分/JS实现轮播图特效/js/script.js
+++ 前端零基础入门/步骤五 让页面动起来的JS基础部分/JS实现轮播图特效/js/script.js
@@ -1,6 +1,39 @@
 // 声明全局变量
 var index = 0;
+// 上一张图片按钮
+var prev = document.getElementById("prev");
+// 下一张图片按钮
+var next = document.getElementById("next");
 
-function byId(id){
-    return typeof(id) === "string"? document.getElementById(id):id;
-}
+// 获取总的图片个数，通过div的数量,获得的是个数组，所以通过length，获取这个数组的长度
+var pics = document.getElementById("banner").getElementsByTagName("div").length;
+
+
+
+// 添加鼠标点击，图片循环事件
+next.addEventListener("click", function (){
+    index++;
+    if (index >= pics){
+        index = 0;
+    }
+});
+
+
+
+// 封装document.getElementById()函数
+// function byId(id){
+//     return typeof(id) === "string"? document.getElementById(id):id;
+// }
+
+// 通用事件绑定方法
+// 使用的时候直接
+// add_handler(next, "click", function (){alert("333")});
+// 就可以了
+// 本次先不用了
+// function add_handler(element, type, handler){
+//     if (element.addEventListener){
+//         element.addEventListener(type, handler, true);
+//     }else {
+//         element["on" + type] = handler;
+//     }
+// }
