048ea021e9ba514591d8316bc9c645d08047f2d3
Marcus <matthw_lew@hotmail.com>
2022-02-16 07:57:19 +0800

    add test

--- 前端零基础入门/步骤五 让页面动起来的JS基础部分/JS实现轮播图特效/js/script.js
+++ 前端零基础入门/步骤五 让页面动起来的JS基础部分/JS实现轮播图特效/js/script.js
@@ -1,22 +1,89 @@
 // 声明全局变量
 var index = 0;
+
+
 // 上一张图片按钮
 var prev = document.getElementById("prev");
 // 下一张图片按钮
 var next = document.getElementById("next");
 
-// 获取总的图片个数，通过div的数量,获得的是个数组，所以通过length，获取这个数组的长度
-var pics = document.getElementById("banner").getElementsByTagName("div").length;
+// 通过banner这个id，和div这个name，获得pics这个DOM，他是个数组
+var pics = document.getElementById("banner").getElementsByTagName("div");
+// 通过length，获取这个数组的长度
+var pics_len = pics.length;
+// 通过dots这个id，和span，获得dots，他是个数组
+var dots = document.getElementById("dots").getElementsByTagName("span");
+// 通过length，获取这个数组的长度
+var dots_len = dots.length;
+
+var timer = null;
 
 
+function stopAutoPlay(){
+    if(timer){
+        clearInterval(startAutoPlay());
+    }
+}
 
-// 添加鼠标点击，图片循环事件
+
+// 自动轮播
+function startAutoPlay(){
+    timer = setInterval(function (){
+        index++;
+        if (index>=3)index = 0;
+        changeImg();
+    }, 5000)
+}
+
+
+function changeImg(){
+    for (let i = 0; i < pics_len; i++) {
+        pics[i].style.display = "none";
+        dots[i].className = "";
+    }
+    pics[index].style.display = "block";
+    dots[index].className = "active";
+}
+
+
+// 点击下一张【next】，图片循环
 next.addEventListener("click", function (){
     index++;
-    if (index >= pics){
+    if (index >= pics_len){
         index = 0;
     }
+    // console.log(index);
+    // todo: 笔记，去掉类，这里得复习一下，怎么去掉类
+    // pics[0].className = "";
+    changeImg();
 });
+
+// 点击上一张【prev】，图片循环
+prev.addEventListener("click", function (){
+    index--;
+    if (index <0) {
+        index = dots_len-1;
+    }
+    // console.log(index);
+    // todo: 笔记，去掉类，这里得复习一下，怎么去掉类
+    // pics[0].className = "";
+    changeImg();
+});
+
+// 给每一个小圆点绑定事件
+for (let i = 0; i < dots.length; i++) {
+    dots[i].setAttribute("data-id",i);
+    dots[i].addEventListener("click", function (){
+        index = this.getAttribute("data-id");
+        changeImg();
+    })
+}
+
+// 自动开启轮播
+startAutoPlay()
+
+
+
 
 
 
