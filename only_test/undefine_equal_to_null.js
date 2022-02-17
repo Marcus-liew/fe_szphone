ae77b22700b5320c24b59850c87e67a4c7ae0869
Marcus <matthw_lew@hotmail.com>
2022-02-17 09:08:34 +0800

    add test

---
+++ only_test/undefine_equal_to_null.js
@@ -0,0 +1,14 @@
+// console.log(undefined == null);
+// console.log(undefined === null);
+// console.log("type of undefined is: " + typeof undefined);
+// console.log("type of null is: " + typeof null);
+// console.log(4);
+// console.log([4]);
+
+
+// 这俩为啥相等？？？
+// https://class.imooc.com/lesson/788#mid=19778
+console.log(typeof 4);
+console.log(typeof [4]);
+
+
